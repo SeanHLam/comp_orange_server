6048dcebac78f4518bd6f84172764d88be33576e
DesmandD3 <dking68@my.bcit.ca>
2022-11-20 13:35:29 -0800

    inital commit

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ app.get('/',(req,res)=>{
     res.end()
 })
 
+app.get('/add-new-user',(req,res)=>{
+    user = req.query.name
+    password = req.query.password
+    //update instead
+    db.run(`INSERT INTO users (name, wins) values (?, ?)`, [user, wins])
+    db.all("SELECT * from users ORDER BY wins DESC", (err, row)=>{
+            console.log(row)
+        })
+    res.end()
+})
+
 app.listen(3001)
