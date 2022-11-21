aaeff4d3a18e3a3393b115a1eaa8b212be586287
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-11-20 20:20:31 -0800

    added tables

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const app = express()
 
 app.use(cors())
 
+//Setting up the tables for users, posts, following, and reports
+//the last three tables use a foreign key to reference a different table to connect them
 db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
 db.run('CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, post varchar(255), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
-db.run('CREATE TABLE IF NOT EXISTS follows(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
+db.run('CREATE TABLE IF NOT EXISTS follows(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
+db.run('CREATE TABLE IF NOT EXISTS reports(id INTEGER PRIMARY KEY AUTOINCREMENT, report varchar(255), tweet_id INTEGER NOT NULL, FOREIGN KEY(tweet_id) REFERENCES posts(id))')
+
 app.get('/',(req,res)=>{
     console.log("server running");
     res.end()
@@ -18,11 +22,10 @@ app.get('/',(req,res)=>{
     });
 })
 
+//gets name and pass from query and adds it to the users table
 app.get('/add-new-user',(req,res)=>{
     userName = req.query.name
     userPassword = req.query.password
-    //update instead
-
     db.run(`INSERT INTO users (name, password) values (?, ?)`, [userName, userPassword])
     db.all("SELECT * from users", (err, row)=>{
             console.log(row)
