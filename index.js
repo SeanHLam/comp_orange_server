9615f89f3333be4b087ed5ee4783ba5b8eaa031a
DesmandD3 <dking68@my.bcit.ca>
2022-11-20 17:07:45 -0800

    inital commit

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const sqlite3 = require('sqlite3').verbose();
 
-app.use(cors())
+const db = new sqlite3.Database('./db');
+
+db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15), password varchar(15))')
+
+app.use(cors());
+
+var userName = "";
+var userPassword = "";
 
 app.get('/',(req,res)=>{
+    console.log("server running");
     res.end()
+    db.each("SELECT * from users", (err,row) => {
+        console.log(row);
+    });
 })
 
 app.get('/add-new-user',(req,res)=>{
-    user = req.query.name
-    password = req.query.password
+    userName = req.query.name
+    userPassword = req.query.password
     //update instead
-    db.run(`INSERT INTO users (name, password) values (?, ?)`, [user, password])
+    db.run(`INSERT INTO users (name, password) values (?, ?)`, [userName, userPassword])
     db.all("SELECT * from users ORDER BY wins DESC", (err, row)=>{
             console.log(row)
         })
