e2e44267296725055bad38b4936f953380c4f578
Minyoung <mbae12@my.bcit.ca>
2022-11-28 13:49:58 -0800

    follow table update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var userPassword = "";
 
 db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
 db.run('CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, post varchar(255), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
-db.run('CREATE TABLE IF NOT EXISTS follows(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
+db.run('CREATE TABLE IF NOT EXISTS follows(id INTEGER PRIMARY KEY AUTOINCREMENT, following_id INTEGER, followed_id INTEGER, FOREIGN KEY(following_id) REFERENCES users(id) FOREIGN KEY(followed_id) REFERENCES users(id))')
 app.get('/',(req,res)=>{
     console.log("server running");
     res.end()
