069ebae4bac37529c90e1a708d53ebdb74b6dd2f
CanSanchez <asanchez24@my.bcit.ca>
2022-12-06 16:02:13 -0800

    added add a post

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var userName = "";
 var userPassword = "";
 
 db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
-db.run('CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, post varchar(255), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
+db.run('CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, post varchar(255), date varchar(255), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
 db.run('CREATE TABLE IF NOT EXISTS follows(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
 db.run('CREATE TABLE IF NOT EXISTS reports(id INTEGER PRIMARY KEY AUTOINCREMENT, report varchar(255), post_id INTEGER NOT NULL, FOREIGN KEY(post_id) REFERENCES posts(id))')
 
@@ -33,9 +33,13 @@ app.get('/',(req,res)=>{
 app.get('/check-user-login', (req, res)=>{
     userName = req.query.name
     userPassword = req.query.password
-    db.each("SELECT name, password FROM users", (err,row) => {
+    db.each("SELECT name, password, id FROM users", (err,row) => {
         console.log(row);
-    });
+    })
+
+    db.each("SELECT * FROM posts", (err,row) => {
+        console.log(row);
+    })
 })
 
 // add a new user to the DB
@@ -49,12 +53,39 @@ app.get('/add-new-user',(req,res)=>{
 })
 
 
+// // add new post to the DB
+// app.post('/add-post',(req, res)=>{
+//     req.body.text
+// })
+
+app.get('/add-post',(req,res)=>{
+
+// db.run('CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, post varchar(255), date varchar(255), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
+
+    console.log(req.query)
+    const post = req.query.post
+    const date = req.query.date
+
+    db.run(`INSERT INTO posts (post, date, user_id) values (?, ?, ?)`, [post, date, 1])
+    //res.end()
+    
+    res.end()
+})
+
+app.get('/posts', (req, res)=>{
+    db.all("SELECT * FROM posts", [], (err,rows) => {
+       return res.json({data:rows});
+    });
+})
+
+
+
 //Adds tweet to report list
 app.get('/send-report', (req, res)=>{
     sentReport = req.query.report
     
     db.each("SELECT report FROM reports", (err,row) => {
-        console.log(row);
+        return res.json(row);
     });
     console.log(sentReport)
     res.end()
