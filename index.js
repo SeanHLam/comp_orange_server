e0c208fa54df0fadb78c7ff75a2bf4fab168f9ce
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-12-07 19:39:06 -0800

    added login get req

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var userName = "";
 var userPassword = "";
 
 db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
-db.run('CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, post varchar(255), date varchar(255), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
+db.run('CREATE TABLE IF NOT EXISTS posts(id INTEGER PRIMARY KEY AUTOINCREMENT, post varchar(255), name varchar(255), date varchar(255), user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES users(id))')
 db.run('CREATE TABLE IF NOT EXISTS follows(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15) UNIQUE, password varchar(255))')
 db.run('CREATE TABLE IF NOT EXISTS reports(id INTEGER PRIMARY KEY AUTOINCREMENT, report varchar(255), post_id INTEGER NOT NULL, FOREIGN KEY(post_id) REFERENCES posts(id))')
 
@@ -33,22 +33,45 @@ app.get('/',(req,res)=>{
 app.get('/check-user-login', (req, res)=>{
     userName = req.query.name
     userPassword = req.query.password
-    db.each("SELECT name, password, id FROM users", (err,row) => {
-        console.log(row);
-    })
+    
+ 
+    db.each("SELECT * from users", (err,row) => {
+        // if (userName === row.name && userPassword === row.password){
+        //     console.log(row.id)
+        //     res.json({row})
+        // }
 
-    db.each("SELECT * FROM posts", (err,row) => {
-        console.log(row);
-    })
+    });
+
+    db.all("SELECT * from users", (err,row) => {
+      res.json({row})
+        
+    });
+   
+
+    // db.each("SELECT name, password, id FROM users", (err,row) => {
+    //     console.log(row);
+    // })
+
+    // db.each("SELECT * FROM posts", (err,row) => {
+    //     console.log(row);
+    // })
+   
 })
 
 // add a new user to the DB
+
 app.get('/add-new-user',(req,res)=>{
     userName = req.query.name
     userPassword = req.query.password
     //update instead
-    db.run(`INSERT INTO users (name, password) values (?, ?)`, [userName, userPassword])
+    db.run(`INSERT OR IGNORE INTO users (name, password) values (?, ?)`, [userName, userPassword])
     //res.end()
+
+    db.each("SELECT * from users", (err,row) => {
+        console.log(row)
+    });
+
     res.json({message:"New user created."})
 })
 
@@ -67,9 +90,18 @@ app.get('/add-post',(req,res)=>{
     const date = req.query.date
 
     db.run(`INSERT INTO posts (post, date, user_id) values (?, ?, ?)`, [post, date, 1])
-    //res.end()
+   
+
+    db.each("SELECT * from posts", (err,row) => {
+        console.log(row)
+    });
+
+    db.all("SELECT * FROM posts", [], (err,rows) => {
+        return res.json({data:rows});
+     });
     
-    res.end()
+    // res.end()
+   
 })
 
 app.get('/posts', (req, res)=>{
@@ -88,7 +120,7 @@ app.get('/send-report', (req, res)=>{
         return res.json(row);
     });
     console.log(sentReport)
-    res.end()
+    //res.end()
 })
 
 
